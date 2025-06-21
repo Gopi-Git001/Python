98d6268a9dc4e8ababee969fd9f7b1ace4542993
Gopi-Git001 <gopi.thungam.001@gmail.com>
2025-06-20 22:38:36 -0500

    string examples

---
+++ 11.py
@@ -0,0 +1,28 @@
+# Write your string index below
+# Start with 'Hello World'
+# and make sure to match spaces and capitalization exactly
+
+mystring = 'TENET'
+print(len(mystring))
+print(mystring[::-1])
+
+x = 'abcdefghijklmnopqrstuvwxyz'
+
+y = 'Hello RamKumar How are you How is your days going '
+
+
+Name = 'Hello world'
+
+print(Name[-3])
+
+print(len(x))
+
+print(x[1:])
+
+print(x[:12:2])
+
+print(x[:20:-3])
+
+print(len(y))
+
+print(y[1:20:3])
