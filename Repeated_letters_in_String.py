c1fa1892d0c9eeb535bdf3462fed1137da87f49a
Gopi-Git001 <gopi.thungam.001@gmail.com>
2025-07-02 12:48:39 -0500

    second commit

--- Repeated_letters_in_String.py
+++ Repeated_letters_in_String.py
@@ -1,10 +1,16 @@
 mystring = 'mississipirmanagarramkumar'
 mylist = tuple(list(mystring))
 print(mylist)
-
-
 newlist  = set([i  for i in mylist if mylist.count(i) >1])
+newlist2  = set([(i,mylist.count(i))  for i in mylist if mylist.count(i) >1])
 print(newlist)
+print(newlist2)
 
 
+existlist = [(i,mylist.count(i)) for i in mylist] 
 
+# print(existlist)
+
+
+# for i in mylist:
+#     print(i,mylist.count(i))
