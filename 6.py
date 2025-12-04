565316878f465aa150d4c3a9bc47e7509e4e895d
GopiRam <gopi.thungam.001@gmail.com>
2025-12-04 16:55:42 -0600

    Dictionaries

---
+++ 6.py
@@ -0,0 +1,12 @@
+D = {'Monday': 16, 'Tuesday': 17,'Wednesday': 18,'Thurday': 19,'Friday': 20,'Saturday': 21,'Sunday': 22 }
+
+print(D.items())
+
+for i,j in D.items():
+    print(i)
+    print(j)
+    
+print(D.keys())
+    
+#three functions for Dictionaries keys ,values and Items  , Dictionaries are also having Mutations 
+
