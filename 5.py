020c583dfae9d14d3675364d091bdd6ede577177
GopiRam <gopi.thungam.001@gmail.com>
2025-12-04 16:43:14 -0600

    list

---
+++ 5.py
@@ -0,0 +1,18 @@
+mylist = [1,'Gopi',0.1]
+
+new_list = [2,4,5]
+
+
+updated_list = mylist+new_list
+
+print(updated_list)
+updated_list.append(5)
+
+print(updated_list)
+
+print(updated_list[::-1])
+
+updated_list.reverse()
+print(updated_list)
+
+# Difference from strings is only Mutation strings are Immutable things and List is Mutate thing 
