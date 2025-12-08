df7447bc87de5ab721dc1b06ecd950f2925022e8
GopiRam <gopi.thungam.001@gmail.com>
2025-12-07 21:00:57 -0600

    args

--- function10.py
+++ function10.py
@@ -1,6 +1,22 @@
+# def myfunc(*args):
+    
+#     return sum(args)
+
+
+# print(myfunc(1,2,3,4,5))
+
+
 def myfunc(*args):
     
-    return sum(args)
+    for i in args:
+        
+        if i%2 ==0:
+            
+            return True
+    else:
+        
+        return False 
+    
 
 
-print(myfunc(1,2,3,4,5))
+print(myfunc(1,5,3))
