29671a68628392f0054e2c9c3168dc166ec11252
GopiRam <gopi.thungam.001@gmail.com>
2025-12-08 14:50:53 -0600

    oops basics

---
+++ oops1.py
@@ -0,0 +1,20 @@
+class Dog():
+    
+    def __init__(self,breed,name,spots):
+        
+        self.breed = breed
+        self.name = name 
+        self.spots = spots 
+        
+
+mydog = Dog(breed='Huskie',name='sammy',spots=True)
+
+
+print(mydog.breed)
+
+print(mydog.name)
+
+print(mydog.spots)
+
+
+
