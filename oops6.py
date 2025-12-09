e4ec8d3323610117f53153ca46799d41a7e0200c
GopiRam <gopi.thungam.001@gmail.com>
2025-12-09 07:34:58 -0600

    try and exceptions

---
+++ oops6.py
@@ -0,0 +1,13 @@
+def ask_a_number():
+    
+    try:
+        number = int(input('Please provide a number'))
+        
+    except:
+        print('That is not a number ')
+        
+    finally :
+        pass
+    
+print(ask_a_number())
+
