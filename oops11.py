bcc8e3929d6295d2bf68da3b790b597c5897d4f5
GopiRam <gopi.thungam.001@gmail.com>
2025-12-11 23:08:23 -0600

    card class

---
+++ oops11.py
@@ -0,0 +1,12 @@
+class Card():
+    
+    def __init__(self,suit,rank):
+        self.rank = rank
+        self.suit = suit
+        
+    def __st__(self):
+        
+        return self.rank +' of '+self.suit
+
+
+    
