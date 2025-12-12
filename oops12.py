e90072e2e024d39dd01844ebd6e59ee817e03209
GopiRam <gopi.thungam.001@gmail.com>
2025-12-11 23:12:35 -0600

    Card class and Deck class

---
+++ oops12.py
@@ -0,0 +1,39 @@
+suits = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
+ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
+values = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, 'Eight':8, 'Nine':9, 'Ten':10, 'Jack':10,
+         'Queen':10, 'King':10, 'Ace':11}
+
+
+import random 
+class Card():
+    
+    def __init__(self,suit,rank):
+        self.rank = rank
+        self.suit = suit
+        
+    def __st__(self):
+        
+        return self.rank +' of '+self.suit
+
+
+class Deck():
+    
+    def __init__(self):
+        
+        self.all_cards = []
+        
+        for suit in suits:
+            for rank in ranks:
+                
+                self.all_cards.append(Card(suit,rank))
+                
+    def shuffle(self):
+        
+        return random.shuffle(self.all_cards)
+    
+    def deal_one(self):
+        
+        return self.all_cards.pop(0)
+    
+    
+    
