43bb3303504e4708eff3440d528bccd3c423a7e7
GopiRam <gopi.thungam.001@gmail.com>
2025-12-14 09:28:01 -0600

    oops concepts 2

--- oops14.py
+++ oops14.py
@@ -25,4 +25,24 @@ class Deck():
     
     def deal_one(self):
         return self.all_cards.pop(0)
+
+class Hand():
     
+    def __init__(self):
+        
+        self.cards = []
+        self.value = 0
+        self.aces = 0
+        
+    def add_cards(self,card):
+        self.cards.append(card)
+        self.value += values[card.rank]
+        
+        if self.aces:
+            self.aces +=1
+            
+        
+    def adjust_aces :
+        
+        
+        
