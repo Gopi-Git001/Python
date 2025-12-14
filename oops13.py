36720eac928b99381d73f7f888b41de825b69328
GopiRam <gopi.thungam.001@gmail.com>
2025-12-13 22:18:14 -0600

    oops concept

--- oops13.py
+++ oops13.py
@@ -99,7 +99,16 @@ while game_on:
     
     player_two_cards.append(player_two.remove_one())
 
-
+    at_war = True
+    
+    while at_war:
+        
+        if player_one_cards[-1].value > player_two_cards[-1].value:
+            player_one.add_cards(player_one_cards)
+            player_one.add_cards(player_two_cards)
+            at_war = False
+            
+        
             
 
 
