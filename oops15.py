117c1e8918df223b5d861760d26ff9820519f90d
GopiRam <gopi.thungam.001@gmail.com>
2025-12-16 23:45:46 -0600

    oops concepts 2

--- oops15.py
+++ oops15.py
@@ -50,9 +50,16 @@ class Hand():
         if card.rank == 'Ace' :
             self.aces += 1
             
-    def adjust_aces(self):
-        
+    def adjust_aces(self):    
         while  self.value >21 and self.aces:
             self.value -=10
             self.aces -= 1
             
+class Chips():
+    
+    def __init__(self):
+        self.total = 100
+        self.bet = 0
+        
+    
+    
