d7057f7b06feb525a2638a342e08052dd88c291f
GopiRam <gopi.thungam.001@gmail.com>
2025-12-16 23:47:06 -0600

    oops concepts 2

--- oops15.py
+++ oops15.py
@@ -61,5 +61,10 @@ class Chips():
         self.total = 100
         self.bet = 0
         
+    def win_bet(self):
+        self.total += self.bet
     
+    def lose_bet(self):
+        self.total -= self.bet 
+        
     
