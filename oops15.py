20cfcc284975a0327cc70ba972275c4b3f3cc196
GopiRam <gopi.thungam.001@gmail.com>
2025-12-16 23:41:02 -0600

    oops concepts 2

--- oops15.py
+++ oops15.py
@@ -39,5 +39,12 @@ class Hand():
     
     def __init__(self):
         
-        
+        self.cards = []
+        self.value = 0
+        self.aces = 0
+
+    def add_cards(self,card):
+        self.cards.append(card)
+        self.value +=values[card.rank] 
+           
         
