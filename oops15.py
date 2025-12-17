f8c4072c50b36d1ba434eb144265c0f591c29a8d
GopiRam <gopi.thungam.001@gmail.com>
2025-12-16 23:36:09 -0600

    oops concepts 2

--- oops15.py
+++ oops15.py
@@ -3,6 +3,8 @@ ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten',
 values = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, 'Eight':8, 'Nine':9, 'Ten':10, 'Jack':10,
          'Queen':10, 'King':10, 'Ace':11}
 
+import random 
+
 class Card():
     
     def __init__(self,suit,rank):
@@ -24,6 +26,9 @@ class Deck():
             for rank in ranks :
                 self.all_cards.append(Card(suit,rank))
                 
+    def shuffle(self):
         
+        return random.shuffle(self.all_cards)
+    
         
         
