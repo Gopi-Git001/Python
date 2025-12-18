d134f01b95208de6ded3433be14b262746555453
GopiRam <gopi.thungam.001@gmail.com>
2025-12-17 23:09:15 -0600

    oops concept

--- oops15.py
+++ oops15.py
@@ -122,6 +122,10 @@ def player_busts(player,dealer,chips):
     print('Player busts')
     chips.lose_bet()
     
+def player_wins(player,dealer,chips):
+    print('Player wins')
+    chips.win_bet()
+    
 
         
          
