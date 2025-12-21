f073397d15abf4b6c2a6e3eb2c46657702062a02
GopiRam <gopi.thungam.001@gmail.com>
2025-12-20 23:03:02 -0600

    oops concept

--- oops15.py
+++ oops15.py
@@ -4,6 +4,7 @@ values = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, 'Eight':8,
          'Queen':10, 'King':10, 'Ace':11}
 
 import random 
+playing = True
 
 class Card():
     
@@ -158,7 +159,8 @@ while game_on:
     
     show_some(player_hand,dealer_hand)
     
-    
+    while playing :
+        
     
     
     
