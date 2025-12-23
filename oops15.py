4042e2fb8a3649a05511b7c88846ebac22026e75
GopiRam <gopi.thungam.001@gmail.com>
2025-12-22 20:59:25 -0600

    oops concept

--- oops15.py
+++ oops15.py
@@ -19,13 +19,13 @@ class Deck():
     
     def __init__(self):
         
-        # self.all_cards = [Card(suit,rank) for suit in suits for rank in ranks]
+         self.all_cards = [Card(suit,rank) for suit in suits for rank in ranks]
         
-        self.all_cards = []
+        # self.all_cards = []
         
-        for suit in suits:
-            for rank in ranks :
-                self.all_cards.append(Card(suit,rank))
+        # for suit in suits:
+        #     for rank in ranks :
+        #         self.all_cards.append(Card(suit,rank))
                 
     def shuffle(self):
         
