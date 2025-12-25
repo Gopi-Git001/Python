9f5cb0b9d8ac081d6fe5a9d5d18bc9234723a92c
GopiRam <gopi.thungam.001@gmail.com>
2025-12-24 23:26:00 -0600

    oops concepts

--- oops16.py
+++ oops16.py
@@ -21,6 +21,11 @@ class Deck():
         
         self.cards = []
         
+        for suit in suits:
+            for rank in ranks :
+                self.cards.append(Card(suit,rank))
+                
+        
         
         
         
