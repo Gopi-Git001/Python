1f12b1d513ad4ac2aeb23b7fb87c47355f25d2f4
Gopi-Git001 <gopi.thungam.001@gmail.com>
2025-12-25 19:52:27 -0600

    oops concept

--- oops17.py
+++ oops17.py
@@ -4,3 +4,15 @@ values = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, 'Eight':8,
          'Queen':10, 'King':10, 'Ace':11}
 
 
+class Card():
+
+    def __init__(self,suit,rank):
+        self.rank = rank 
+        self.suit = suit
+
+    def __str__(self):
+        return f"{self.rank} of {self.suit} "
+
+my_cards = Card('Hearts','Two')
+
+print(my_cards.rank)
