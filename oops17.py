e344909dbbf9061b98d75d978953c52f6a3ceb16
Gopi-Git001 <gopi.thungam.001@gmail.com>
2025-12-25 19:57:31 -0600

    oops concept

--- oops17.py
+++ oops17.py
@@ -24,7 +24,14 @@ class Card():
 
 class Deck():
     def __init__(self):
-        self.all_cards = [Card(suit,rank) for suit in suits for rank in ranks ]
+        # self.all_cards = [Card(suit,rank) for suit in suits for rank in ranks ]
+
+        self.all_cards = []
+
+        for suit in suits:
+            for rank in ranks:
+                self.all_cards.append(Card(suit,rank))
+                
 
 
 new_deck = Deck()
