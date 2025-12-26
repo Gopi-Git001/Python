ad8c79e0100928588bdc095c477bb619d0841d93
Gopi-Git001 <gopi.thungam.001@gmail.com>
2025-12-25 19:59:09 -0600

    oops concept

--- oops17.py
+++ oops17.py
@@ -36,6 +36,9 @@ class Deck():
     def shuffle(self):
         return random.shuffle(self.all_cards)
     
+    def deal_one(self):
+        return self.all_cards.pop(0)
+    
 
 new_deck = Deck()
 
