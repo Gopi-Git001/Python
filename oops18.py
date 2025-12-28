fccf42dc8407fb1018bb2f4d2227b9c865a95ad0
GopiRam <gopi.thungam.001@gmail.com>
2025-12-27 21:42:39 -0600

    oops concepts

--- oops18.py
+++ oops18.py
@@ -41,7 +41,18 @@ class Hand():
         self.cards.append(card)
         self.value += values[card.rank]
         
+        if card.rank == 'ace':
+            self.aces +=1
+            
+    def adjust_aces(self):
+        while self.value >21 and self.aces :
+            self.value -= 10
+            self.aces -= 1
+            
 
+
+        
+            
       
 
         
