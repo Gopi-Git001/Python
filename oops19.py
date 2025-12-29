cbbb4f49d8b17d041ec099b338db51f6f7098dbf
GopiRam <gopi.thungam.001@gmail.com>
2025-12-28 23:32:38 -0600

    oops concept

--- oops19.py
+++ oops19.py
@@ -5,3 +5,8 @@ values = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, 'Eight':8,
 
 import random 
 playing = True
+
+class Card():
+    def __init__(self,suit,rank):
+        self.suit = suit 
+        self.rank = rank 
