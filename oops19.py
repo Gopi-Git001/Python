0b9c9a893d75bdf4c36a171302fa1d4b5cf354ea
GopiRam <gopi.thungam.001@gmail.com>
2025-12-28 23:33:53 -0600

    oops concept

--- oops19.py
+++ oops19.py
@@ -17,3 +17,7 @@ class Card():
 class Deck():
     def __init__(self):
         self.all_cards = []
+        
+        for suit in suits:
+            for rank in ranks:
+                self.all_cards.append(Card(suit,rank))
