4e6df9772491181e4f055108ffaa6b1951d75b5d
GopiRam <gopi.thungam.001@gmail.com>
2025-12-29 17:57:30 -0600

    AI content

---
+++ AI_1.py
@@ -0,0 +1,27 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+api_key = os.getenv("OPENAI_API_KEY")
+
+if api_key:
+    print("Key existed in the path file")
+else:
+    print('No Key found')
+    
+from openai import OpenAI
+
+openai = OpenAI()
+
+messages = [{'role':"user","content":"Hi"}]
+
+response = openai.chat.completions.content(
+    model = "gpt-40-mini",
+    messages = messages
+)
+
+answer = response.choices[0].message.content 
+
+print(answer)
+
