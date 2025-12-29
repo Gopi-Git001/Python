e5917f6783baf3721972dd507f21409ac0bb9aa8
GopiRam <gopi.thungam.001@gmail.com>
2025-12-28 23:38:27 -0600

    oops concept

--- oops19.py
+++ oops19.py
@@ -45,3 +45,10 @@ class Hand():
         while self.value >21 and self.aces :
             self.value -= 10
             self.aces -= 1
+            
+class Chips():
+    
+    def __init__(self):
+        self.total = 100
+        self.bet = 0
+        
