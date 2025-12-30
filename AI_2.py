307d57a290896e7b7473cbd2b3ac9c1673e26115
GopiRam <gopi.thungam.001@gmail.com>
2025-12-29 18:09:46 -0600

    AI content

---
+++ AI_2.py
@@ -0,0 +1,36 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+from openai import OpenAI
+
+openai = OpenAI()
+
+prompt_1 = "Please propose a hard, challenging question to assess someone's IQ. Respond only with the question."
+
+messages = [{'role':"user","content":prompt_1}]
+
+response = openai.chat.completions.content(
+    model = "gpt-40-mini",
+    messages = messages
+)
+
+question = response.choices[0].message.content 
+
+print(question)
+
+
+messages = [{'role':'user','content':question}]
+
+response = openai.chat.completions.content(
+    model = 'gpt-40-mini',
+    messages = messages
+    
+)
+
+answer = response.choices[0].message.content 
+
+print(answer)
+
+
