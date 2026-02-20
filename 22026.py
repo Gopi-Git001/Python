940d85af72e0f6be1985655fd34bc67aaef7d7bf
GopiRam <gopi.thungam.001@gmail.com>
2026-02-19 18:09:06 -0600

    add one more project

--- 22026.py
+++ 22026.py
@@ -1,33 +1,15 @@
-mylist = [1,2,3,4,5,6,7,8,9]
 
-print(len(mylist))
+mydict = {'Key1':1,'key2':2,3:4,5:3.4674}
 
-print(mylist[::])
+print(mydict.items())
 
-print(mylist[::2])
-
-print(mylist[::-1])
+print(mydict.keys())
 
 
-mylist[0] = 4674
+mydict['4.5']=62748
 
-print(mylist)
-
-#properties
+print(mydict)
 
 
-mylist.append(62748)
-
-print(mylist)
-#we can add pop item using indexing
-mylist.pop()
-
-print(mylist)
-
-
-mynewlist = [11,22,33]
-
-mylist.extend(mynewlist)
-
-print(mylist)
+print(mydict['Key1'])
 
