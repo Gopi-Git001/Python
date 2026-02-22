d27724e311a7df1860346ffb06f1cc638437689a
GopiRam <gopi.thungam.001@gmail.com>
2026-02-21 20:51:07 -0600

    add one more project

--- 22026.py
+++ 22026.py
@@ -1,6 +1,8 @@
 #List Comprehension
 mylist = [1,2,3,4,5,6,7,8,9]
 print([i for i in mylist if i%2 ==0])
+
+#Function calling
 def myfunc(name):   
     return ''.join([char.upper() if i%2 ==0  else char.lower() for i ,char in enumerate(name)])
 print(myfunc('gopiram'))
