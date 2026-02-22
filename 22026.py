298863d602d82eb4765ec218fb050b4395082136
GopiRam <gopi.thungam.001@gmail.com>
2026-02-21 20:48:53 -0600

    add one more project

--- 22026.py
+++ 22026.py
@@ -4,6 +4,5 @@ print([i for i in mylist if i%2 ==0])
 def myfunc(name):
     
     return ''.join([char.upper() if i%2 ==0  else char.lower() for i ,char in enumerate(name)])
-
 print(myfunc('gopiram'))
 
